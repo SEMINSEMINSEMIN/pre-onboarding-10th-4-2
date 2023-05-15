505e4b32ec7a68e8758580b62698d7aeddbe4389
강세민 <ksm9802@gmail.com>
2023-05-15 18:27:17 +0900

    feat: 추천 기능 debounce #21
    * debounce를 훅으로 구현
      * 이유: input의 value가 state로 관리됨. input의 value는 onChange마다 업데이트 돼야하지만, 추천어 검색 요청은 디바운싱을 이용해 해야함. input의 value는 onChange마다 바로 바로 업데이트 되고, value state가 업데이트 됐을 때 useEffect를 이용해 검색 요청을 하기 위해 유틸 함수가 아닌 훅을 사용함
    
    * debounce 시간 상수화 -> useDebounce 훅의 delay 파라미터 기본값으로 사용

--- src/components/InputTodo.tsx
+++ src/components/InputTodo.tsx
@@ -8,8 +8,11 @@ import React, {
 } from "react";
 
 import { createTodo } from "../api/todo";
-import { InputTodoPropsType } from "../types/todo";
 import { searchRecommendation } from "../api/search";
+
+import useDebounce from "../hooks/useDebounce";
+
+import { InputTodoPropsType } from "../types/todo";
 
 const InputTodo = forwardRef<HTMLInputElement, InputTodoPropsType>(
   ({ setTodos, setFocus }, ref: Ref<HTMLInputElement>) => {
@@ -19,6 +22,20 @@ const InputTodo = forwardRef<HTMLInputElement, InputTodoPropsType>(
     useEffect(() => {
       setFocus();
     }, [setFocus]);
+
+    const showRecommendation = useCallback(async (text: string) => {
+      const { data: recommendData } = await searchRecommendation(text, 1);
+
+      // TODO: recommendData를 드롭다운에 보여줘야 함.
+      // 아래 코드 삭제 예정
+      console.info(recommendData);
+    }, []);
+
+    useDebounce(inputText, showRecommendation);
+
+    const handleInpChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputText(e.target.value);
+    };
 
     const handleSubmit = useCallback(
       async (e: React.FormEvent<HTMLFormElement>) => {
@@ -30,14 +47,6 @@ const InputTodo = forwardRef<HTMLInputElement, InputTodoPropsType>(
           if (!trimmed) {
             return alert("Please write something");
           }
-
-          // Search API 작동 확인용 코드 시작: 추후 삭제 예정
-          const { data: recommendData } = await searchRecommendation(
-            "lorem",
-            1
-          );
-          console.info(recommendData.result);
-          // Search API 작동 확인용 코드 끝
 
           const newItem = { title: trimmed };
           const { data } = await createTodo(newItem);
@@ -64,7 +73,7 @@ const InputTodo = forwardRef<HTMLInputElement, InputTodoPropsType>(
           placeholder="Add new todo..."
           ref={ref}
           value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
+          onChange={handleInpChange}
           disabled={isLoading}
         />
         {!isLoading ? (
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,3 @@
 export const MAX_SUGGESTIONS = 10;
+
+export const DEBOUNCE_TIME = 500;
